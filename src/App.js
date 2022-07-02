32e2d79f76ecd3adc7f0ce289f6649bc2e64502b
Administrator <admin@example.com>
2022-07-02 08:37:36 -0500

    feat/baner listo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AuthState from './context/auth/authState'
 import RouterMain from './routers/RouterMain'
+import './styles/index.css'
 
 const App = () => {
   return (
--- src/routers/RouterMain.js
+++ src/routers/RouterMain.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Header from '../modules/general/screen/Header'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import RutaPrivada from '../modules/helpers/RutaPrivada'
 import GeneralCreateService from '../modules/general/screen/GeneralCreateService'
 import CarsScreen from '../modules/cars/screens/CarsScreen'
@@ -8,20 +7,20 @@ import HealthScreen from '../modules/health/screens/HealthScreen'
 import HomeScreen from '../modules/home/screens/HomeScreen'
 import GeneralScreen from '../modules/general/screen/GeneralScreen'
 import AuthScreen from '../modules/auth/screen/AuthScreen'
+import HeaderScreen from '../modules/general/screen/HeaderScreen'
 
 const RouterMain = () => {
   return (
     <>
-        <Header />
-        <Routes>
-            <Route path='/customer' element={<RutaPrivada><GeneralCreateService /></RutaPrivada>} />
-            <Route path='/login' element={<AuthScreen />} />
-            <Route path='/home' element={<HomeScreen />} />
-            <Route path='/health' element={<HealthScreen />} />
-            <Route path='/cars' element={<CarsScreen />} />
-            <Route path='/*' element={<GeneralScreen />} />
-
-        </Routes>
+      <HeaderScreen />
+      <Routes>
+        <Route path='/customer' element={<RutaPrivada><GeneralCreateService /></RutaPrivada>} />
+        <Route path='/login' element={<AuthScreen />} />
+        <Route path='/home' element={<HomeScreen />} />
+        <Route path='/health' element={<HealthScreen />} />
+        <Route path='/cars' element={<CarsScreen />} />
+        <Route path='/*' element={<GeneralScreen />} />
+      </Routes>
     </>
   )
 }

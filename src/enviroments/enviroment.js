9726b21ecb51598897218a8ef116b3416f21dd15
Administrator <admin@example.com>
2022-07-04 19:11:50 -0500

    feat/se cambio de captcha

--- src/enviroments/enviroment.js
+++ src/enviroments/enviroment.js
@@ -1,5 +1,6 @@
 
 export const URL_BACKEND = 'https://backendservices2022.herokuapp.com'
 
-export const COD_RECAPTCHA_V3 = '6LctEpsgAAAAACjoFLsBfEK3AI5M8lLoZ75p4KhL'
+export const COD_RECAPTCHA_V3 = '6Le-McQgAAAAAPlgx_Suecak4fZBUuYfhAA2EQYX'
+// export const COD_RECAPTCHA_V3 = '6LctEpsgAAAAACjoFLsBfEK3AI5M8lLoZ75p4KhL'
 
